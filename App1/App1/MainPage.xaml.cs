1b5c8dc8bf021a7d1aa48f34ca3073234d14379c
AaQib Bajwa <55548887+bajwaaaqib@users.noreply.github.com>
2019-09-26 12:55:58 +0500

    Xmarin Crossplate form mobile application
    Adding two values from user

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_btn(object sender, EventArgs e)
+        {
+            double first = double.Parse(value1.Text);
+            double second = double.Parse(value2.Text);
+            result.Text =(first + second).ToString();
+
+        }
+    }
+}
